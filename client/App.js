39bbbc3b756d29c2c29f0531b21e1cd1169d312a
Juan Carlos Pazos Mandiá <jucapaman@gmail.com>
2018-01-21 00:21:55 +0100

    Some views and some functions

--- client/App.js
+++ client/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
-import CityFinder from './cities/CityFinder'
+import Home from './container/Home'
 
 // App component - represents the whole app
 class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <header></header>
-        
-        <CityFinder/>
-      </div>
-    );
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      view: 1,
+      city: null
+    }
+
+    this.selectCity = this.selectCity.bind(this)
   }
+
+
+  selectCity(city){
+    this.setState({city: city})
+    if(this.state.city != null) {
+      this.setState({view: 2})
+    }
+  }
+
+  render() {
+    switch(this.state.view){
+      case 1:
+       return <Home selectCity={this.selectCity}/>
+      default:
+        return <div>{this.state.city}</div>
+    }
+  }
+
 }
 
 export default App
